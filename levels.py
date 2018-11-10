8aabe98a67ead4743f734a9e000a68a2411defab
Martin Brom <martinbrom97@gmail.com>
2018-11-10 20:17:03 +0100

    balancing

--- levels.py
+++ levels.py
@@ -4,6 +4,6 @@ enemies = [
     Enemy(max_health=200, damage_time=10000, name='Gorgon'),
     Enemy(max_health=200, damage_time=8000, name='Dragon'),
     Enemy(max_health=800, damage_time=6000, name='Lorem'),
-    Enemy(max_health=1600, damage_time=5000, damage=150, name='Hydra'),
-    Enemy(max_health=2000, damage_time=3000, damage=150, name='Cthulhu'),
+    Enemy(max_health=1000, damage_time=5000, damage=150, name='Hydra'),
+    Enemy(max_health=1000, damage_time=3000, damage=150, name='Cthulhu'),
 ]
--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
         self.experience = 0
         self.max_health = 1000
         self.health = 1000
-        self.base_damage = 1000
+        self.base_damage = 1500
         self.recovery_time = 1000  # ms
 
     def hit(self, amount):
